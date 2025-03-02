1b353d3438c48870c0b048f39f83b26576f1d77d
vm3do <medoavvadi@gmail.com>
2025-03-02 16:05:29 +0000

    setting up the migration, factories and seeders

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->decimal('salary', 10, 2);
+            $table->date('salary_date');
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
