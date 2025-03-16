4edeeab80c71247edb579e82d13da86273c10cbe
vm3do <medoavvadi@gmail.com>
2025-03-16 23:56:12 +0000

    fetching expense details to edit modal

--- resources/views/user/expenses.blade.php
+++ resources/views/user/expenses.blade.php
@@ -217,7 +217,15 @@
                                                 </div>
                                             </div>
                                             <div class="flex gap-1">
-                                                <button x-data @click="$dispatch('open-modal', 'edit-autopay')" class="p-2 hover:bg-[#FF6B6B]/10 rounded-lg transition-colors">
+                                                <button onclick="fillModal(this)" x-data @click="$dispatch('open-modal', 'edit-autopay')" class="p-2 hover:bg-[#FF6B6B]/10 rounded-lg transition-colors"
+                                                    data-id="{{$expense->id}}"
+                                                    data-name="{{$expense->name}}"
+                                                    data-amount="{{$expense->amount}}"
+                                                    data-date="{{$expense->date}}"
+                                                    data-category="{{$expense->category->name}}"
+                                                    data-is-recurring="{{$expense->is_recurring}}"
+                                                    data-frequency="{{$expense->frequecy}}"
+                                                >
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                                         class="w-4 h-4 text-[#FF6B6B]">
@@ -321,13 +329,14 @@
                                         </td>
                                         <td class="py-4 px-4">
                                             <div class="flex items-center justify-end gap-2" x-data @click="$dispatch('open-modal', 'edit-expense')" >
-                                                <button class="p-2 hover:bg-[--accent-light] rounded-lg transition-colors"
+                                                <button onclick="fillModal(this)" class="p-2 hover:bg-[--accent-light] rounded-lg transition-colors"
                                                     data-id="{{$expense->id}}"
                                                     data-name="{{$expense->name}}"
                                                     data-amount="{{$expense->amount}}"
                                                     data-date="{{$expense->date}}"
                                                     data-category="{{$expense->category->name}}"
                                                     data-is-recurring="{{$expense->is_recurring}}"
+                                                    data-frequency="{{$expense->frequecy}}"
                                                     >
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -349,45 +358,7 @@
                                     </tr>
                                     
                                 @endforeach
-                                <tr class="hover:bg-[--accent-light]/30 transition-colors">
-                                    <td class="py-4 px-4">
-                                        <div class="flex items-center gap-3">
-                                            <span class="text-sm font-medium text-[--neutral-900]">Feb 19, 2024</span>
-                                        </div>
-                                    </td>
-                                    <td class="py-4 px-4">
-                                        <span class="text-sm text-[--neutral-900]">Netflix Subscription</span>
-                                    </td>
-                                    <td class="py-4 px-4">
-                                        <span
-                                            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-purple-100 text-purple-800">
-                                            Entertainment
-                                        </span>
-                                    </td>
-                                    <td class="py-4 px-4">
-                                        <span class="text-sm font-medium text-[--neutral-900]">120 DH</span>
-                                    </td>
-                                    <td class="py-4 px-4">
-                                        <div class="flex items-center justify-end gap-2">
-                                            <button class="p-2 hover:bg-[--accent-light] rounded-lg transition-colors">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="w-5 h-5 text-[--accent]">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                                </svg>
-                                            </button>
-                                            <button class="p-2 hover:bg-red-50 rounded-lg transition-colors">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="w-5 h-5 text-red-500">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>
+
                             </tbody>
                         </table>
                     </div>
@@ -734,6 +705,21 @@
         document.getElementById('rangeInput').addEventListener('input', () => {
             document.getElementById('rangeValue').textContent = document.getElementById('rangeInput').value + '%'
         })
+
+        function fillForm(button){
+            document.getElementById('test').value = button.getAttribute('data-name');
+            document.getElementById('test').value = button.getAttribute('data-amount');
+            document.getElementById('test').value = button.getAttribute('data-date');
+            document.getElementById('test').value = button.getAttribute('data-category');
+            document.getElementById('test').value = button.getAttribute('data-is-recurring');
+            document.getElementById('test').value = button.getAttribute('data-is-frequency');
+
+            let id = button.getAttribute('data-id');
+
+            let form = document.getElementById('edit-form');
+
+            form.action = `/expenses/update/${id}`
+        }
     </script> 
 </body>
 
