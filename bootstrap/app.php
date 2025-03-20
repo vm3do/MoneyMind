d10633a3bbdd937b959f4794512a2ddce18f94d5
vm3do <medoavvadi@gmail.com>
2025-03-20 11:31:28 +0000

    set the middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(['is_admin' => AdminMiddleware::class]);
+        $middleware->alias(['is_user' => UserMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StatisticController;
 use App\Mail\Email;
-use App\Models\Alert;
-use App\Models\Category;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Catch_;
@@ -25,7 +23,7 @@ Route::get('/', function () {
 // email test
 
 
-Route::get('/test-email', function(){
+Route::get('/test-email', function () {
     $name = 'Ayadi';
     $emailmessage = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, omnis? Corporis dolorem officiis unde quidem illum dolor neque aliquam nesciunt!';
     $response = Mail::to('mailtrap@test.com')->send(new Email($name, $emailmessage));
@@ -33,26 +31,30 @@ Route::get('/test-email', function(){
 });
 
 
-Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
-Route::post('/dashboard/category/store', [CategoryController::class, 'store'])->name('category.store');
-Route::put('/dashboard/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
-Route::delete('/dashboard/category/destroy/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::post('/dashboard/category/store', [CategoryController::class, 'store'])->name('category.store');
+    Route::put('/dashboard/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+    Route::delete('/dashboard/category/destroy/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+});
 
-Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
-Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
-Route::put('/expenses/update/{expense}', [ExpenseController::class, 'update']);
-Route::delete('/expenses/delete/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+Route::middleware(['auth', 'is_user'])->group(function () {
+    Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+    Route::put('/expenses/update/{expense}', [ExpenseController::class, 'update']);
+    Route::delete('/expenses/delete/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
 
-Route::post('/expenses/alert', [AlertController::class, 'store'])->name('alert.store');
-Route::put('/expenses/alert/{id}', [AlertController::class, 'update'])->name('alert.update');
+    Route::post('/expenses/alert', [AlertController::class, 'store'])->name('alert.store');
+    Route::put('/expenses/alert/{id}', [AlertController::class, 'update'])->name('alert.update');
 
-Route::get('/statistics', [StatisticController::class, 'index'])->name('statistics.index');
-Route::put('/statistics/update/{id}', [StatisticController::class, 'update']);
+    Route::get('/statistics', [StatisticController::class, 'index'])->name('statistics.index');
+    Route::put('/statistics/update/{id}', [StatisticController::class, 'update']);
 
-Route::middleware('auth')->group(function () {
+    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
